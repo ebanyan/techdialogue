d21719eb978f6f5a7cb4534afd201678fa102ae8
Satish Krishnaraj <satishkn4@gmail.com>
2021-03-13 11:31:09 -0600

    Initial commit

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,6 +8,11 @@ import Layout from "../layouts/index.js"
 export default function Contact({ location }) {
     return (
         <Layout location={location}>
+            <p>
+                <h2> About Tech Dialogue:</h2>
+                <h3> Summary </h3>
+            </p>
+
             <div>
                 <ArenguForm
                     id="161521760729681752"
